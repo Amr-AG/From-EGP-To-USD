07567c1729e14d4153a9a231bdcc56732e52c5d3
Amr <82913751+Amr-AG@users.noreply.github.com>
2022-08-22 03:23:13 +0200

    Update learn.js

--- JS/learn.js
+++ JS/learn.js
@@ -1,28 +1,52 @@
-let egp = document.querySelector("form .egp");
-let usd = document.querySelector("form .usd");
-let conv = document.querySelector(".convert");
+let from = document.querySelector("form .from");
+let to = document.querySelector("form .to");
+let selectFrom = document.querySelector(".select-from ");
+let selectTo = document.querySelector(".select-To ");
+console.log(from);
+console.log(to);
+console.log(selectFrom);
+console.log(selectTo);
 
-console.log(egp);
-console.log(usd);
-console.log(conv);
+if (selectFrom.value === "EGP") {
+  from.onchange = function () {
+    if (from.value < 0) {
+      from.value = 0;
+      usd.value = 0;
+      preventdefault();
+    } else {
+      to.value = (from.value / 19.14).toFixed(2);
+    }
+    return;
+  };
+}
 
-egp.onchange = function () {
-  if (egp.value <= 0 && egp.value === "") {
-    egp.value = 0;
-    usd.value = 0;
-    preventdefault();
-  } else {
-    usd.value = (egp.value / 19.14).toFixed(2);
+selectFrom.addEventListener("change", () => {
+  if (selectFrom.value === "EGP") {
+    selectTo.value = "USD";
+    from.onchange = function () {
+      if (from.value < 0) {
+        from.value = 0;
+        usd.value = 0;
+        preventdefault();
+      } else {
+        to.value = (from.value / 19.14).toFixed(2);
+      }
+      return;
+    };
   }
-  return;
-};
-conv.onclick = function () {
-  if (egp.value <= 0 && egp.value === "") {
-    egp.value = 0;
-    usd.value = 0;
-    preventdefault();
-  } else {
-    usd.value = (egp.value / 19.14).toFixed(2);
+
+  if (selectFrom.value === "USD") {
+    selectTo.value = "EGP";
+    from.onchange = function () {
+      if (from.value < 0) {
+        from.value = 0;
+        to.value = 0;
+        preventdefault();
+      } else {
+        to.value = (from.value * 19.14).toFixed(2);
+      }
+      return;
+    };
   }
-  return;
-};
+});
+
